3e171ab87aa078ce13d741ed3979e46f1a8f712e
Andrea Manzi <manzolo@libero.it>
2018-11-24 16:57:23 +0100

    add tests

--- tests/Cdf/BiCoreBundle/Controller/TestTablesNotAuthorizedControllerTest.php
+++ tests/Cdf/BiCoreBundle/Controller/TestTablesNotAuthorizedControllerTest.php
@@ -13,11 +13,26 @@ class TestTablesNotAuthorizedControllerTest extends FifreeWebtestcaseNorolesAuth
             'Magazzino', 'Ordine');
         foreach ($routes as $route) {
             $client = $this->client;
-            $url = $client->getContainer()->get('router')->generate($route."_container");
+            $url = $client->getContainer()->get('router')->generate($route . "_container");
             //$this->assertContains('DoctrineORMEntityManager', get_class($em));
 
             $client->request('GET', $url);
             $this->assertEquals(403, $client->getResponse()->getStatusCode());
         }
     }
+//    public function testSecuredNotAuthorizedTabella()
+//    {
+//        $routes = array('Cliente', 'Fornitore', 'Prodottofornitore',
+//            'Magazzino', 'Ordine');
+//        foreach ($routes as $route) {
+//            $client = $this->client;
+//            $url = $client->getContainer()->get('router')->generate($route . "_tabella");
+//            //$this->assertContains('DoctrineORMEntityManager', get_class($em));
+//
+//            $crawler = $client->request('GET', $url);
+//            $parametri = $this->getParametriTabella($route, $crawler);
+//
+//            $this->assertEquals(403, $client->getResponse()->getStatusCode());
+//        }
+//    }
 }
