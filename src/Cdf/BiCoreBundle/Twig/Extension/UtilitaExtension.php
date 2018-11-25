d119f8874213e4e2e78ac9345e522ffe99bdbb7c
Andrea Manzi <manzolo@libero.it>
2018-11-25 14:53:44 +0100

    refactor

--- src/Cdf/BiCoreBundle/Twig/Extension/UtilitaExtension.php
+++ src/Cdf/BiCoreBundle/Twig/Extension/UtilitaExtension.php
@@ -2,29 +2,55 @@
 
 namespace Cdf\BiCoreBundle\Twig\Extension;
 
-use Cdf\BiCoreBundle\Controller\FiUtilita;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class UtilitaExtension extends \Twig_Extension
 {
-
     /**
      * {@inheritdoc}
      */
+    protected $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
     public function getFunctions()
     {
         return array(
+            new \Twig_SimpleFunction('json_decode', array($this, 'jsonDecode', 'is_safe' => array('html'))),
             new \Twig_SimpleFunction('db2data', array($this, 'getDb2data', 'is_safe' => array('html'))),
             new \Twig_SimpleFunction('remote_file_exists', array($this, 'remoteFileExists', 'is_safe' => array('html'))),
+            new \Twig_SimpleFunction('serviceExists', array($this, 'serviceExists')),
+            new \Twig_SimpleFunction('getParameter', array($this, 'getParameter')),
         );
     }
+    
+    public function getParameter($parameter)
+    {
+        if ($this->container->hasParameter($parameter)) {
+            return $this->container->getParameter($parameter);
+        } else {
+            return '';
+        }
+    }
 
+    public function serviceExists($service)
+    {
+        return $this->container->has($service);
+    }
+    
     public function getDb2data($giorno)
     {
         // highlight_string highlights php code only if '<?php' tag is present.
 
         return FiUtilita::db2data($giorno, true);
     }
-
+    public function jsonDecode($string)
+    {
+        return json_decode($string);
+    }
     /**
      * @param string $url
      *
