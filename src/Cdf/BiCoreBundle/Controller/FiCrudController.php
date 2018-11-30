1963a3d62423dd16de612bf95d0fcbba61e9b88d
Andrea Manzi <manzolo@libero.it>
2018-12-01 00:30:58 +0100

    refactor FiController

--- src/Cdf/BiCoreBundle/Controller/FiCrudController.php
+++ src/Cdf/BiCoreBundle/Controller/FiCrudController.php
@@ -22,159 +22,6 @@ class FiCrudController extends AbstractController
     protected $permessi;
 
     /**
-     * Lists all tables entities.
-     */
-    public function index(Request $request, Packages $assetsmanager)
-    {
-        $bundle = $this->getBundle();
-        $controller = $this->getController();
-        $idpassato = $request->get('id');
-
-        if (!$this->getPermessi()->canRead()) {
-            throw new AccessDeniedException("Non si hanno i permessi per visualizzare questo contenuto");
-        }
-        $crudtemplate = $this->getCrudTemplate($bundle, $controller, $this->getThisFunctionName());
-
-        $entityclassnotation = $this->getEntityClassNotation();
-        $entityclass = $this->getEntityClassName();
-
-        $formclass = str_replace("Entity", "Form", $entityclass);
-
-        $modellocolonne = array(
-                /*
-                  $controller . ".nominativo" => array(
-                  "nometabella" => $controller,
-                  "nomecampo" => "nominativo",
-                  "etichetta" => "Nominativo",
-                  "ordine" => 10,
-                  "larghezza" => 200,
-                  "escluso" => false
-                  ),
-                  $controller . ".datanascita" => array(
-                  "nometabella" => $controller,
-                  "nomecampo" => "datanascita",
-                  "etichetta" => "Data di nascita",
-                  "ordine" => 20,
-                  "larghezza" => 100,
-                  "escluso" => false
-                  ),
-
-                 */
-        );
-
-        $colonneordinamento = array($controller . '.id' => "DESC");
-        $filtri = array();
-        $prefiltri = array();
-        $entityutils = new \Cdf\BiCoreBundle\Utils\Entity\EntityUtils($this->get("doctrine")->getManager());
-        $tablenamefromentity = $entityutils->getTableFromEntity($entityclass);
-        $colonneordinamento = array($tablenamefromentity . '.id' => "DESC");
-        $parametritabella = array("em" => ParametriTabella::setParameter("default"),
-            'tablename' => ParametriTabella::setParameter($tablenamefromentity),
-            'nomecontroller' => ParametriTabella::setParameter($controller),
-            'bundle' => ParametriTabella::setParameter($bundle),
-            'entityname' => ParametriTabella::setParameter($entityclassnotation),
-            'entityclass' => ParametriTabella::setParameter($entityclass),
-            'formclass' => ParametriTabella::setParameter($formclass),
-            'modellocolonne' => ParametriTabella::setParameter(json_encode($modellocolonne)),
-            'permessi' => ParametriTabella::setParameter(json_encode($this->getPermessi())),
-            'urltabella' => ParametriTabella::setParameter($assetsmanager->getUrl('/') . $controller . '/' . 'tabella'),
-            'baseurl' => ParametriTabella::setParameter($assetsmanager->getUrl('/')),
-            'idpassato' => ParametriTabella::setParameter($idpassato),
-            'titolotabella' => ParametriTabella::setParameter("Elenco " . $controller),
-            'multiselezione' => ParametriTabella::setParameter("0"),
-            'editinline' => ParametriTabella::setParameter("0"),
-            'paginacorrente' => ParametriTabella::setParameter("1"),
-            'paginetotali' => ParametriTabella::setParameter(""),
-            'righetotali' => ParametriTabella::setParameter("0"),
-            'righeperpagina' => ParametriTabella::setParameter("15"),
-            'estraituttirecords' => ParametriTabella::setParameter("0"),
-            'colonneordinamento' => ParametriTabella::setParameter(json_encode($colonneordinamento)),
-            'filtri' => ParametriTabella::setParameter(json_encode($filtri)),
-            'prefiltri' => ParametriTabella::setParameter(json_encode($prefiltri)),
-            'traduzionefiltri' => ParametriTabella::setParameter(""),
-        );
-
-        return $this->render($crudtemplate, array('parametritabella' => $parametritabella,));
-    }
-
-    private function getParametroIndexDettaglio($parametripassati, $keyparametro, $defaultvalue)
-    {
-        if (isset($parametripassati[$keyparametro])) {
-            $parametro = $parametripassati[$keyparametro];
-        } else {
-            $parametro = $defaultvalue;
-        }
-        return $parametro;
-    }
-
-    /**
-     * Lists all tables entities.
-     */
-    public function indexDettaglio(Request $request, Packages $assetsmanager)
-    {
-        if (!$this->getPermessi()->canRead()) {
-            throw new AccessDeniedException("Non si hanno i permessi per visualizzare questo contenuto");
-        }
-
-        $bundle = $this->getBundle();
-        $controller = $this->getController();
-        $parametripassati = json_decode($request->get('parametripassati'), true);
-
-        $filtri = $this->getParametroIndexDettaglio($parametripassati, "filtri", array());
-        $prefiltri = $this->getParametroIndexDettaglio($parametripassati, "prefiltri", array());
-        $titolotabella = $this->getParametroIndexDettaglio($parametripassati, "titolotabella", "Elenco " . $controller);
-        $modellocolonne = $this->getParametroIndexDettaglio($parametripassati, "modellocolonne", array());
-        $colonneordinamento = $this->getParametroIndexDettaglio($parametripassati, "colonneordinamento", array());
-        $multiselezione = $this->getParametroIndexDettaglio($parametripassati, "multiselezione", 0);
-        $parametriform = $this->getParametroIndexDettaglio($parametripassati, "parametriform", array());
-
-        $template = $bundle . ':' . $controller . ':' . $this->getThisFunctionName() . '.html.twig';
-        if (!$this->get('templating')->exists($template)) {
-            $template = $controller . '/Crud/' . $this->getThisFunctionName() . '.html.twig';
-        }
-
-        $entityclassnotation = $this->getEntityClassNotation();
-        $entityclass = $this->getEntityClassName();
-
-        $formclass = str_replace("Entity", "Form", $entityclass);
-
-        $entityutils = new \Cdf\BiCoreBundle\Utils\Entity\EntityUtils($this->get("doctrine")->getManager());
-
-        $tablenamefromentity = $entityutils->getTableFromEntity($entityclass);
-        $parametritabella = array("em" => ParametriTabella::setParameter("default"),
-            'tablename' => ParametriTabella::setParameter($tablenamefromentity),
-            'nomecontroller' => ParametriTabella::setParameter($controller),
-            'bundle' => ParametriTabella::setParameter($bundle),
-            'entityname' => ParametriTabella::setParameter($entityclassnotation),
-            'entityclass' => ParametriTabella::setParameter($entityclass),
-            'formclass' => ParametriTabella::setParameter($formclass),
-            'parametriform' => ParametriTabella::setParameter(json_encode($parametriform)),
-            'modellocolonne' => ParametriTabella::setParameter(json_encode($modellocolonne)),
-            'permessi' => ParametriTabella::setParameter(json_encode($this->getPermessi())),
-            'urltabella' => ParametriTabella::setParameter($assetsmanager->getUrl('/') . $controller . '/' . 'tabella'),
-            'baseurl' => ParametriTabella::setParameter($assetsmanager->getUrl('/')),
-            'idpassato' => ParametriTabella::setParameter(0),
-            'titolotabella' => ParametriTabella::setParameter($titolotabella),
-            'multiselezione' => ParametriTabella::setParameter($multiselezione),
-            'editinline' => ParametriTabella::setParameter("0"),
-            'paginacorrente' => ParametriTabella::setParameter("1"),
-            'paginetotali' => ParametriTabella::setParameter(""),
-            'righeperpagina' => ParametriTabella::setParameter("15"),
-            'colonneordinamento' => ParametriTabella::setParameter(json_encode($colonneordinamento)),
-            'filtri' => ParametriTabella::setParameter(json_encode($filtri)),
-            'prefiltri' => ParametriTabella::setParameter(json_encode($prefiltri)),
-            'traduzionefiltri' => ParametriTabella::setParameter(""),
-        );
-
-        return $this->render(
-            $template,
-            array(
-                    'parametritabella' => $parametritabella,
-                        )
-        );
-    }
-
-    /**
      * Displays a form to create a new table entity.
      */
     public function new(Request $request)
@@ -358,6 +205,89 @@ class FiCrudController extends AbstractController
         );
     }
 
+    private function checkAggiornaRight($id)
+    {
+        if ($id === 0) {
+            if (!$this->getPermessi()->canCreate()) {
+                throw new AccessDeniedException("Non si hanno i permessi per creare questo contenuto");
+            }
+        } else {
+            if (!$this->getPermessi()->canUpdate()) {
+                throw new AccessDeniedException("Non si hanno i permessi per modificare questo contenuto");
+            }
+        }
+    }
+
+    /**
+     * Inline existing table entity.
+     */
+    public function aggiorna(Request $request, $id)
+    {
+        $this->checkAggiornaRight($id);
+
+        /* @var $em \Doctrine\ORM\EntityManager */
+        $controller = $this->getController();
+        $entityclass = $this->getEntityClassName();
+        $this->checkAggiornaRight($id);
+        $em = $this->getDoctrine()->getManager();
+        $queryBuilder = $em->createQueryBuilder();
+        $insert = ($id === 0);
+
+        if ($insert) {
+            //Insert
+            $entity = new $entityclass();
+            $queryBuilder
+                    ->insert($entityclass)
+            ;
+        } else {
+            //Update
+            $entity = $em->getRepository($entityclass)->find($id);
+            if (!$entity) {
+                throw $this->createNotFoundException('Impossibile trovare l\'entità ' . $controller . ' per il record con id ' . $id);
+            }
+            $queryBuilder
+                    ->update($entityclass, "u")
+                    ->where("u.id = :id")
+                    ->setParameter("id", $id);
+        }
+        $values = $request->get("values");
+        $querydaeseguire = false;
+
+        foreach ($values as $value) {
+            $fieldpieces = explode(".", $value["fieldname"]);
+            $table = $fieldpieces[0];
+            //Si prende in considerazione solo i campi strettamente legati a questa entity
+            if ($table == $controller && count($fieldpieces) == 2) {
+                $field = $fieldpieces[1];
+                $subfieldpieces = explode("_", $field);
+                if ($insert) {
+                    $queryBuilder->setValue($field, ':' . $field);
+                    $queryBuilder->setParameter($field, $value["fieldvalue"]);
+                    $querydaeseguire = true;
+                } else {
+                    /**/
+                    $nomefunzioneget = "get";
+                    foreach ($subfieldpieces as $field4get) {
+                        $nomefunzioneget .= ucfirst($field4get);
+                    }
+                    if (method_exists($entity, $nomefunzioneget)) {
+                        if ($entity->$nomefunzioneget() != $value["fieldvalue"]) {
+                            $querydaeseguire = true;
+                            $queryBuilder->set("u." . $field, ':' . $field);
+                            $queryBuilder->setParameter($field, $value["fieldvalue"]);
+                        }
+                    }
+                }
+            } else {
+                continue;
+            }
+        }
+        if ($querydaeseguire) {
+            $queryBuilder->getQuery()->execute();
+        }
+
+        return new \Symfony\Component\HttpFoundation\JsonResponse(array("errcode" => 0, "message" => "Registrazione eseguita"));
+    }
     /**
      * Deletes a table entity.
      */
