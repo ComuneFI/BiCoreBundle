d80409f8d3225749f07bbbcb5a4694b35b7c0ea0
Andrea Manzi <manzolo@libero.it>
2018-12-15 11:51:17 +0100

    refactor command

--- src/Cdf/BiCoreBundle/Command/BiCoreBundlePubblicamanualeCommand.php
+++ src/Cdf/BiCoreBundle/Command/BiCoreBundlePubblicamanualeCommand.php
@@ -6,34 +6,41 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Finder\Finder;
+use Symfony\Component\Filesystem\Filesystem;
 
 class BiCoreBundlePubblicamanualeCommand extends Command
 {
 
     protected static $defaultName = 'bicorebundle:pubblicamanuale';
-    
+
     protected function configure()
     {
         $this
                 ->setDescription('Copia il manuale dalla cartella Doc alla cartella Web')
                 ->setHelp('Estende la pubblicazione degli assets al manuale');
     }
+    public function __construct($projectdir, Filesystem $fs)
+    {
+        $this->projectdir = $projectdir;
+        $this->fs = $fs;
 
+        // you *must* call the parent constructor
+        parent::__construct();
+    }
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->copiaManuale();
-    }
+        $projectDir = $this->projectdir;
+        $manuale = "manuale.pdf";
+        $originDir = $projectDir . DIRECTORY_SEPARATOR . 'doc' . DIRECTORY_SEPARATOR . 'manuale';
+        $manualepath = $originDir . DIRECTORY_SEPARATOR . $manuale;
+        if ($this->fs->exists($manualepath)) {
+            $targetDir = $projectDir . DIRECTORY_SEPARATOR . 'public';
 
-    protected function copiaManuale()
-    {
-        $filesystem = $this->getContainer()->get('filesystem');
-
-        $projectDir = $this->getContainer()->get('kernel')->getProjectDir() .DIRECTORY_SEPARATOR . '..' ;
-        $originDir = $projectDir . '/doc/manuale';
-        $targetDir = $projectDir . '/public';
-
-        $filesystem->mkdir($targetDir, 0777);
-        //    // We use a custom iterator to ignore VCS files
-        $filesystem->mirror($originDir, $targetDir, Finder::create()->name('manuale.pdf')->in($originDir));
+            $this->fs->mkdir($targetDir, 0777);
+            //    // We use a custom iterator to ignore VCS files
+            $this->fs->mirror($originDir, $targetDir, Finder::create()->name($manuale)->in($originDir));
+        } else {
+            throw new \Exception("Attenzione, non è presente il file " . $manualepath);
+        }
     }
 }
