17acf66d668242f6eff088f6aa4a45082d57d8eb
Andrea Manzi <andrea.manzi@comune.fi.it>
2019-04-15 17:09:29 +0200

    fix svg notify icon

--- src/Cdf/BiCoreBundle/Resources/public/js/bibootstrap.js
+++ src/Cdf/BiCoreBundle/Resources/public/js/bibootstrap.js
@@ -4,8 +4,8 @@ class BiNotification {
         var elem = document.createElement('div');
         elem.setAttribute("id", 'binotificationcontainer');
         document.body.appendChild(elem);
-
-        $(elem).html('<div class="notification with-icon ' + tipo + '" role="alert" aria-labelledby="' + messaggio + '" id="binotification"><h5><svg class="icon"><use xlink:href="' + baseUrl + 'bundles/bicore/svg/sprite.svg#' + icon + '"><\/use><\/svg>' + messaggio + '<\/h5><\/div>');
+        console.log(window.baseUrl);
+        $(elem).html('<div class="notification with-icon ' + tipo + '" role="alert" aria-labelledby="' + messaggio + '" id="binotification"><h5><svg class="icon"><use xlink:href="' + window.baseUrl + '/bundles/bicore/svg/sprite.svg#' + icon + '"><\/use><\/svg>' + messaggio + '<\/h5><\/div>');
         notificationShow('binotification', 6000);
     }
 }
