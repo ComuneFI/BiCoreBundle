f104d2b5f16f295f8294acdba969b66f9c265570
Andrea Manzi <manzolo@libero.it>
2019-04-19 19:30:35 +0200

    remove jquery from test

--- tests/App/Functional/FunctionalOrdineControllerTest.php
+++ tests/App/Functional/FunctionalOrdineControllerTest.php
@@ -1,11 +1,11 @@
 <?php
+
 namespace App\Tests\Controller;
 
 use Cdf\BiCoreBundle\Tests\Utils\BiTestAuthorizedClient;
 
 class FunctionalOrdineControllerTest extends BiTestAuthorizedClient
 {
-
     public function testFunctionalOrdineIndex()
     {
         $ordiniregistrati = 14;
@@ -17,13 +17,12 @@ class FunctionalOrdineControllerTest extends BiTestAuthorizedClient
         $this->assertSame(self::$baseUri . $testUrl, $client->getCurrentURL()); // Assert we're still on the same page
         $ordini = $crawler->filterXPath('//table[@id="' . $htmltableid . '"]')->filter('tbody')->filter('tr')->each(function ($tr, $i) {
             return $tr->filter('td')->each(function ($td, $i) {
-                    return trim($td->text());
-                });
+                        return trim($td->text());
+                    });
         });
         $this->assertSame($ordiniregistrati, count($ordini));
         $this->logout();
     }
-
     public function testFunctionalOrdineEditinline()
     {
         $clientiregistrati = 15;
@@ -37,7 +36,10 @@ class FunctionalOrdineControllerTest extends BiTestAuthorizedClient
         $this->executeScript("document.getElementById('ParametriOrdine').dataset.editinline= 'Ma=='");
         $this->pressButton('.tabellarefresh');
         sleep(1);
-        $this->dblClickElement(".bibottonimodificatabellaOrdine[data-biid=\"9\"]");
+        $this->clickElement('.bibottonimodificatabellaOrdine[data-biid="9"]');
+        $client->waitFor('a.h-100.d-flex.align-items-center.it-file');
+        $this->clickElement('a.h-100.d-flex.align-items-center.it-file');
+        //$this->dblClickElement(".bibottonimodificatabellaOrdine[data-biid=\"9\"]");
         //$this->executeScript("$('.bibottonimodificatabellaOrdine[data-biid=\"9\"]').dblclick();");
         sleep(2);
         $selectorinputqta = 'tr[data-bitableid=\"9\"] > td[data-nomecampo="Ordine.quantita"] :input';
@@ -45,7 +47,10 @@ class FunctionalOrdineControllerTest extends BiTestAuthorizedClient
 
         $qta1ex = 21;
 
-        $this->executeScript("$('".$selectorinputqta."').val(".$qta1ex.')');
+        //$this->executeScript("$('".$selectorinputqta."').val(".$qta1ex.')');
+        sleep(1);
+        $this->executeScript("document.querySelector('#tableOrdine > tbody > tr:nth-child(2) > td:nth-child(4) > div > input').value=".$qta1ex);
+
         //$this->executeScript("document.getElementById('" . $selectorinputqta . "').value = " . $qta1ex . '');
         sleep(1);
         //$this->executeScript("$('".$selectorconfirm."').click()");
@@ -55,17 +60,30 @@ class FunctionalOrdineControllerTest extends BiTestAuthorizedClient
         /* qui */
         $ordinerow = $this->em->getRepository('App:Ordine')->find(9);
         $this->assertEquals($qta1ex, $ordinerow->getQuantita());
-
-        $this->executeScript('$(".tabellarefresh").click();');
+        $this->clickElement('.tabellarefresh');
+        //$this->executeScript('$(".tabellarefresh").click();');
         sleep(1);
 
         $qta2ex = 22;
-        $this->executeScript("$('.bibottonimodificatabellaOrdine[data-biid=\"9\"]').dblclick();");
-        sleep(2);
-        $this->executeScript("$('" . $selectorinputqta . "').val(" . $qta2ex . ')');
+        //$this->executeScript("$('.bibottonimodificatabellaOrdine[data-biid=\"9\"]').dblclick();");
+        $this->clickElement('.bibottonimodificatabellaOrdine[data-biid="9"]');
+        $client->waitFor('a.h-100.d-flex.align-items-center.it-file');
+        $this->clickElement('a.h-100.d-flex.align-items-center.it-file');
+        
+//        sleep(2);
+//        $this->executeScript("$('" . $selectorinputqta . "').val(" . $qta2ex . ')');
+//        sleep(1);
+//        $this->executeScript("$('" . $selectorconfirm . "').click()");
+//        sleep(1);
+                //$this->executeScript("$('".$selectorinputqta."').val(".$qta1ex.')');
+        $this->executeScript("document.querySelector('#tableOrdine > tbody > tr:nth-child(2) > td:nth-child(4) > div > input').value=".$qta2ex);
+
+        //$this->executeScript("document.getElementById('" . $selectorinputqta . "').value = " . $qta1ex . '');
         sleep(1);
-        $this->executeScript("$('" . $selectorconfirm . "').click()");
+        //$this->executeScript("$('".$selectorconfirm."').click()");
+        $this->clickElement($selectorconfirm);
         sleep(1);
+
 
         /* qui */
         $this->em->clear();
