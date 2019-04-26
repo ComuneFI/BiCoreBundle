14561fe4c9176fc55a6b35d09a1a05d2f429f636
Andrea Manzi <manzolo@libero.it>
2019-04-26 13:21:43 +0200

    refactor tests

--- tests/Cdf/BiCoreBundle/Entity/PermessiEntityTest.php
+++ tests/Cdf/BiCoreBundle/Entity/PermessiEntityTest.php
@@ -2,12 +2,12 @@
 
 namespace Cdf\BiCoreBundle\Tests\Entity;
 
-namespace Cdf\BiCoreBundle\Tests\Entity;
-
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Cdf\BiCoreBundle\Entity\Permessi;
 
 class PermessiEntityTest extends KernelTestCase
 {
+
     /**
      * @var \Doctrine\ORM\EntityManager
      */
@@ -24,17 +24,23 @@ class PermessiEntityTest extends KernelTestCase
                 ->getManager()
         ;
     }
-
-    public function testSearchBy()
+    public function testSearchByPermssi()
     {
         $object = $this->em
-                ->getRepository('BiCoreBundle:Permessi')
+                ->getRepository(Permessi::class)
                 ->findByCrud('cru')
         ;
 
         $this->assertCount(1, $object);
     }
-
+    public function testSearchByModuloPermssi()
+    {
+        $object = $this->em
+                ->getRepository(Permessi::class)
+                ->findPermessoModuloOperatore("Magazzino", "userreadroles")
+        ;
+        $this->assertTrue($object->getCrud() === "r");
+    }
     /**
      * {@inheritdoc}
      */
