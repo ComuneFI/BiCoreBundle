e678c3615e907d6bde517b4596bc8474a19f9fb7
Andrea Manzi <manzolo@libero.it>
2019-09-21 18:05:16 +0200

    refactor tabella

--- src/Cdf/BiCoreBundle/Controller/FiCoreTabellaControllerTrait.php
+++ src/Cdf/BiCoreBundle/Controller/FiCoreTabellaControllerTrait.php
@@ -11,6 +11,7 @@ use Cdf\BiCoreBundle\Utils\Export\TabellaXls;
 
 trait FiCoreTabellaControllerTrait
 {
+
     private $tabellaxls;
 
     public function tabella(Request $request)
@@ -18,90 +19,52 @@ trait FiCoreTabellaControllerTrait
         if (!$this->permessi->canRead($this->getController())) {
             throw new AccessDeniedException('Non si hanno i permessi per visualizzare questo contenuto');
         }
-        $doctrine = $this->get('doctrine');
-        //$em = $doctrine->getManager();
 
         $parametripassati = array_merge($request->get('parametri'), array('user' => $this->getUser()));
         $parametriform = isset($parametripassati['parametriform']) ?
                 json_decode(ParametriTabella::getParameter($parametripassati['parametriform']), true) : array();
-        $configurazionetabella = new Tabella($doctrine, $parametripassati);
-        $parametritabella = array(
-            'parametritabella' => $configurazionetabella->getConfigurazionecolonnetabella(),
-            'recordstabella' => $configurazionetabella->getRecordstabella(),
-            'paginacorrente' => $configurazionetabella->getPaginacorrente(),
-            'paginetotali' => $configurazionetabella->getPaginetotali(),
-            'righetotali' => $configurazionetabella->getRighetotali(),
-            'traduzionefiltri' => $configurazionetabella->getTraduzionefiltri(),
-        );
         $classbundle = ParametriTabella::getParameter($parametripassati['entityclass']);
-        //$formbundle = $namespace . '\\' . $bundle . 'Bundle' . '\\Form\\' . $controller;
         $formbundle = ParametriTabella::getParameter($parametripassati['formclass']);
-        $formType = $formbundle.'Type';
+        $formType = $formbundle . 'Type';
 
         $entity = new $classbundle();
-        //$tablename = ParametriTabella::getParameter($parametripassati["tablename"]);
         $controller = ParametriTabella::getParameter($parametripassati['nomecontroller']);
         $form = $this->createForm(
             $formType,
             $entity,
             array('attr' => array(
-                        'id' => 'formdati'.$controller,
+                        'id' => 'formdati' . $controller,
                     ),
-                    'action' => $this->generateUrl($controller.'_new'),
+                    'action' => $this->generateUrl($controller . '_new'),
                     'parametriform' => $parametriform,
                 )
         );
 
-        $parametri = array_merge($parametripassati, $parametritabella);
+        $parametri = array_merge($parametripassati, $this->getParametriTabella($parametripassati));
         $parametri['form'] = $form->createView();
-
-        $template = $controller.'/Tabella/tabellacontainer.html.twig';
-        $templatelocation = $controller.'/';
-        if (!$this->get('templating')->exists($template)) {
-            $template = 'BiCoreBundle:'.$controller.':Tabella/tabellacontainer.html.twig';
-            $templatelocation = 'BiCoreBundle:'.$controller.':';
-            if (!$this->get('templating')->exists($template)) {
-                $templatelocation = 'BiCoreBundle:Standard:';
-                $template = $templatelocation.'Tabella/tabellacontainer.html.twig';
-            }
-        }
-        $parametri['templatelocation'] = $templatelocation;
+        $templateobj = $this->getTabellaTemplateInformations($controller);
+        $parametri['templatelocation'] = $templateobj["path"];
 
         return $this->render(
-            $template,
-            array(
-                            'parametri' => $parametri,
-                        )
+            $templateobj["template"],
+            array('parametri' => $parametri)
         );
     }
-
     public function setTabellaxls(TabellaXls $tabellaxls)
     {
         $this->tabellaxls = $tabellaxls;
     }
-
     public function exportXls(Request $request)
     {
-        $doctrine = $this->get('doctrine');
-
         try {
             $parametripassati = array_merge($request->get('parametri'), array('user' => $this->getUser()));
             $parametripassati['estraituttirecords'] = ParametriTabella::setParameter('1');
-            $configurazionetabella = new Tabella($doctrine, $parametripassati);
-            $parametritabella = array(
-                'parametritabella' => $configurazionetabella->getConfigurazionecolonnetabella(),
-                'recordstabella' => $configurazionetabella->getRecordstabella(),
-                'paginacorrente' => $configurazionetabella->getPaginacorrente(),
-                'paginetotali' => $configurazionetabella->getPaginetotali(),
-                'righetotali' => $configurazionetabella->getRighetotali(),
-                'traduzionefiltri' => $configurazionetabella->getTraduzionefiltri(),
-                'nomecontroller' => ParametriTabella::getParameter($parametripassati['nomecontroller']),
-            );
-            $filexls = $this->tabellaxls->esportaexcel($parametritabella);
+
+            $filexls = $this->tabellaxls->esportaexcel($this->getParametriTabellaXls($parametripassati));
             if (file_exists($filexls)) {
                 $response = array(
                     'status' => '200',
-                    'file' => 'data:application/vnd.ms-excel;base64,'.base64_encode(file_get_contents($filexls)),
+                    'file' => 'data:application/vnd.ms-excel;base64,' . base64_encode(file_get_contents($filexls)),
                 );
                 @unlink($filexls);
             } else {
@@ -113,10 +76,53 @@ trait FiCoreTabellaControllerTrait
         } catch (\Exception $exc) {
             $response = array(
                 'status' => '500',
-                'file' => $exc->getFile().' -> Riga: '.$exc->getLine().' -> '.$exc->getMessage(),
+                'file' => $exc->getFile() . ' -> Riga: ' . $exc->getLine() . ' -> ' . $exc->getMessage(),
             );
         }
 
         return new JsonResponse($response);
+    }
+    protected function getParametriTabella(array $parametripassati)
+    {
+        $doctrine = $this->get('doctrine');
+        $configurazionetabella = new Tabella($doctrine, $parametripassati);
+        $parametritabella = array(
+            'parametritabella' => $configurazionetabella->getConfigurazionecolonnetabella(),
+            'recordstabella' => $configurazionetabella->getRecordstabella(),
+            'paginacorrente' => $configurazionetabella->getPaginacorrente(),
+            'paginetotali' => $configurazionetabella->getPaginetotali(),
+            'righetotali' => $configurazionetabella->getRighetotali(),
+            'traduzionefiltri' => $configurazionetabella->getTraduzionefiltri()
+        );
+        return $parametritabella;
+    }
+    protected function getParametriTabellaXls(array $parametripassati)
+    {
+        $doctrine = $this->get('doctrine');
+        $configurazionetabella = new Tabella($doctrine, $parametripassati);
+        $parametritabella = array(
+            'parametritabella' => $configurazionetabella->getConfigurazionecolonnetabella(),
+            'recordstabella' => $configurazionetabella->getRecordstabella(),
+            'paginacorrente' => $configurazionetabella->getPaginacorrente(),
+            'paginetotali' => $configurazionetabella->getPaginetotali(),
+            'righetotali' => $configurazionetabella->getRighetotali(),
+            'traduzionefiltri' => $configurazionetabella->getTraduzionefiltri(),
+            'nomecontroller' => ParametriTabella::getParameter($parametripassati['nomecontroller']),
+        );
+        return $parametritabella;
+    }
+    protected function getTabellaTemplateInformations($controller)
+    {
+        $template = $controller . '/Tabella/tabellacontainer.html.twig';
+        $path = $controller . '/';
+        if (!$this->get('templating')->exists($template)) {
+            $template = 'BiCoreBundle:' . $controller . ':Tabella/tabellacontainer.html.twig';
+            $path = 'BiCoreBundle:' . $controller . ':';
+            if (!$this->get('templating')->exists($template)) {
+                $path = 'BiCoreBundle:Standard:';
+                $template = $path . 'Tabella/tabellacontainer.html.twig';
+            }
+        }
+        return array("path" => $path, "template" => $template);
     }
 }
