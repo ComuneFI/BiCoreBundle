99be85f195a88a3d17450bcdbbbc75d16138c718
Andrea Manzi <manzolo@libero.it>
2019-12-03 20:45:54 +0100

    fix deprecation

--- src/Cdf/PannelloAmministrazioneBundle/DependencyInjection/Configuration.php
+++ src/Cdf/PannelloAmministrazioneBundle/DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder('fi_pannello_amministrazione');
-        $rootnode = $treeBuilder->getRootNode();
+        $treeBuilder->getRootNode();
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
