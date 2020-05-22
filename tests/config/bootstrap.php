45102bb45711e79659079d68e8715889102b50ed
Andrea Manzi <andrea.manzi@comune.fi.it>
2020-05-22 23:38:11 +0200

    refactor tests

--- tests/config/bootstrap.php
+++ tests/config/bootstrap.php
@@ -2,7 +2,7 @@
 
 use Symfony\Component\Dotenv\Dotenv;
 
-require dirname(__DIR__).'/vendor/autoload.php';
+require dirname(__DIR__).'/../vendor/autoload.php';
 
 if (!array_key_exists('APP_ENV', $_SERVER)) {
     $_SERVER['APP_ENV'] = $_ENV['APP_ENV'] ?? null;
@@ -49,3 +49,6 @@ if ('prod' !== $_SERVER['APP_ENV']) {
 $_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = $_SERVER['APP_ENV'] ?: $_ENV['APP_ENV'] ?: 'dev';
 $_SERVER['APP_DEBUG'] = $_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? 'prod' !== $_SERVER['APP_ENV'];
 $_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = (int) $_SERVER['APP_DEBUG'] || filter_var($_SERVER['APP_DEBUG'], FILTER_VALIDATE_BOOLEAN) ? '1' : '0';
+
+//date_default_timezone_set('Europe/Rome');
+//\Cdf\BiCoreBundle\Tests\Utils\BiTest::cleanFilesystem();
