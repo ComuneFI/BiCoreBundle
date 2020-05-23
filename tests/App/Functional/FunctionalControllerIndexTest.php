a0387956b629e09f77f1736eecb5fdae567054c3
Andrea Manzi <andrea.manzi@comune.fi.it>
2020-05-23 10:30:18 +0200

    rename tests

--- tests/App/Functional/FunctionalControllerIndexTest.php
+++ tests/App/Functional/FunctionalControllerIndexTest.php
@@ -4,7 +4,7 @@ namespace App\Tests\Controller;
 
 use Cdf\BiCoreBundle\Tests\Utils\BiTestAuthorizedClient;
 
-class FunctionalControllerTest extends BiTestAuthorizedClient
+class FunctionalControllerIndexTest extends BiTestAuthorizedClient
 {
     public function testFunctionalClienteIndex()
     {
@@ -145,6 +145,24 @@ class FunctionalControllerTest extends BiTestAuthorizedClient
             });
         });
         $this->assertSame($ordiniregistrati, count($ordini));
+        $this->logout();
+    }
+
+    public function testFunctionalProdottofornitoreIndex()
+    {
+        $prodottifornitoreregistrati = 11;
+        $htmltableid = 'tableProdottofornitore';
+        $client = static::createPantherClient();
+        $testUrl = '/Prodottofornitore/';
+        $crawler = $client->request('GET', $testUrl);
+        $client->waitFor('#'.$htmltableid); // Wait for the tabellaProdottofornitore to appear
+        $this->assertSame(self::$baseUri.$testUrl, $client->getCurrentURL()); // Assert we're still on the same page
+        $prodottifornitore = $crawler->filterXPath('//table[@id="'.$htmltableid.'"]')->filter('tbody')->filter('tr')->each(function ($tr, $i) {
+            return $tr->filter('td')->each(function ($td, $i) {
+                return trim($td->text());
+            });
+        });
+        $this->assertSame($prodottifornitoreregistrati, count($prodottifornitore));
         $this->logout();
     }
 
