3848e5bf7c4d71cedaabb1782762d3c0895df77e
Andrea Manzi <manzolo@libero.it>
2020-10-25 23:52:10 +0100

    road to symfony 5.x

--- src/Cdf/BiCoreBundle/Controller/FiApiController.php
+++ src/Cdf/BiCoreBundle/Controller/FiApiController.php
@@ -7,7 +7,7 @@ use function count;
 use ReflectionClass;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Twig\Environment;
-use Symfony\Component\Inflector\Inflector;
+use Symfony\Component\String\Inflector\EnglishInflector;
 use Cdf\BiCoreBundle\Utils\Api\ApiUtils;
 use Cdf\BiCoreBundle\Utils\String\StringUtils;
 
@@ -87,7 +87,8 @@ class FiApiController extends AbstractController
         if (isset($this->inflectorExceptions[$singleForm])) {
             return $this->inflectorExceptions[$singleForm];
         }
-        return Inflector::pluralize($singleForm);
+        $inflector = new EnglishInflector();
+        return $inflector->pluralize($singleForm);
     }
 
     /**
