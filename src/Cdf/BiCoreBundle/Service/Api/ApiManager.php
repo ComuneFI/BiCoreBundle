4369f4f4f471ad3b43cc37ca8e3dffb1cc768773
Vannetti Riccardo <riccardo.vannetti@comune.fi.it>
2023-10-23 08:00:10 +0000

    Update file ApiManager.php

--- src/Cdf/BiCoreBundle/Service/Api/ApiManager.php
+++ src/Cdf/BiCoreBundle/Service/Api/ApiManager.php
@@ -152,12 +152,18 @@ class ApiManager
     }
 
     /**
-     * Set the token managed by wso2TokenService into api-service
+     * Set the token managed by oauth2TokenService into api-service
+     * It forces the refresh of token
      * If token is already set, it replaces it
      */
     private function refreshToken(mixed $api, string $token): void
     {
         if ($this->isOauth2()) {
+            //call oauth2 service for a new token
+            $this->oauth2Service->refreshToken();
+            //get the new token
+            $token = $this->oauth2Service->getToken();
+            //set the new token to be used by api
             $api->getConfig()->setAccessToken($token);
         }
     }
@@ -178,14 +184,11 @@ class ApiManager
 
 
     /**
-     * Retry one more time if exception is for broken pipes
+     * Retry one more time the already done call to API objects
      */
-    private function retry(Exception $apiEx, object $object, string $method, mixed $args): mixed
+    private function retry(object $object, string $method, mixed $args): mixed
     {
-        if ($this->isBrokenPipe($apiEx)) {
-            return $object->$method(...$args);
-        }
-        throw $apiEx;
+        return $object->$method(...$args);
     }
 
     /**
@@ -206,7 +209,7 @@ class ApiManager
                 $amount = $tools['api']->$getCountmethod(...$arguments);
         } catch (\Exception $apiEx) {
             $this->refreshToken($tools['api'], $token);
-            $amount = $this->retry($apiEx, $tools['api'], $getCountmethod, $arguments);
+            $amount = $this->retry($tools['api'], $getCountmethod, $arguments);
         }
         return $amount;
     }
@@ -234,7 +237,7 @@ class ApiManager
                 $results = $tools['api']->$getAllmethod(...$arguments);
         } catch (\Exception $apiEx) {
             $this->refreshToken($tools['api'], $token);
-            $results = $this->retry($apiEx, $tools['api'], $getAllmethod, $arguments);
+            $results = $this->retry($tools['api'], $getAllmethod, $arguments);
         }
         return $results;
     }
@@ -262,7 +265,7 @@ class ApiManager
             $results = $tools['api']->$getMethod(...$arguments);
         } catch (\Exception $apiEx) {
             $this->refreshToken($tools['api'], $token);
-            $results = $this->retry($apiEx, $tools['api'], $getMethod, $arguments);
+            $results = $this->retry($tools['api'], $getMethod, $arguments);
         }
         return $results;
     }
@@ -287,7 +290,7 @@ class ApiManager
             $results = $tools['api']->$getAllmethod(...$arguments);
         } catch (\Exception $apiEx) {
             $this->refreshToken($tools['api'], $token);
-            $results = $this->retry($apiEx, $tools['api'], $getAllmethod, $arguments);
+            $results = $this->retry($tools['api'], $getAllmethod, $arguments);
         }
         return $results;
     }
@@ -333,7 +336,7 @@ class ApiManager
             $results = $tools['api']->$createMethod(...$arguments);
         } catch (\Exception $apiEx) {
             $this->refreshToken($tools['api'], $token);
-            $results = $this->retry($apiEx, $tools['api'], $createMethod, $arguments);
+            $results = $this->retry($tools['api'], $createMethod, $arguments);
         }
         return $results;
     }
@@ -358,7 +361,7 @@ class ApiManager
             $results = $tools['api']->$createMethod(...$arguments);
         } catch (\Exception $apiEx) {
             $this->refreshToken($tools['api'], $token);
-            $results = $this->retry($apiEx, $tools['api'], $createMethod, $arguments);
+            $results = $this->retry($tools['api'], $createMethod, $arguments);
         }
         return $results;
     }
